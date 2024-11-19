d5fcdfe1906b062d4811364df2112380e15ef5e0
Shaz-Momin <shazmomin2012@gmail.com>
2024-11-19 02:28:48 -0600

    combined adv sets and trained them for 1 epoch (batch size 128)

---
+++ adv_combined/adv_combined_generator.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+def combine_files(combined_file):
+    folders = ['./adv_addSent', './adv_addAny', './adv_addCommon']
+    # Define the paths to the files
+    validation_files = [folder + "/" + combined_file + ".jsonl" for folder in folders]
+
+    # Combine the data from all files
+    combined_data = []
+
+    for file_path in validation_files:
+        with open(file_path, 'r') as file:
+            for line in file:
+                combined_data.append(json.loads(line))
+
+    # Save the combined data to the new file
+    with open("./adv_combined/" + combined_file + ".jsonl", 'w') as outfile:
+        for entry in combined_data:
+            json.dump(entry, outfile)
+            outfile.write('\n')
+
+# Combine the files
+combine_files("train")
