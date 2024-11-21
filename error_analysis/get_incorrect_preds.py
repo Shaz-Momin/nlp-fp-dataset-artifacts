233f33b2201b0975b995cd0bfbde4a587b6f3073
Shaz-Momin <shazmomin2012@gmail.com>
2024-11-21 08:24:19 -0600

    calculated error categories on fine-tuned_v2 model & plot the results

--- error_analysis/get_incorrect_preds.py
+++ error_analysis/get_incorrect_preds.py
@@ -10,9 +10,18 @@ ADD_SENT_EVAL_OUTPUT = './error_analysis/addSent_incorrect_pred.jsonl'
 ADD_ANY_EVAL_OUTPUT = './error_analysis/addAny_incorrect_pred.jsonl'
 ADD_COMMON_EVAL_OUTPUT = './error_analysis/addCommon_incorrect_pred.jsonl'
 
+ADD_SENT_EVAL_INPUT_V2 = './new_eval_outputs/adv_combined_v2/adv_addSent/eval_predictions.jsonl'
+ADD_ANY_EVAL_INPUT_V2 = './new_eval_outputs/adv_combined_v2/adv_addAny/eval_predictions.jsonl'
+ADD_COMMON_EVAL_INPUT_V2 = './new_eval_outputs/adv_combined_v2/adv_addCommon/eval_predictions.jsonl'
+
+ADD_SENT_EVAL_OUTPUT_V2 = './new_eval_outputs/adv_combined_v2/addSent_incorrect_pred.jsonl'
+ADD_ANY_EVAL_OUTPUT_V2 = './new_eval_outputs/adv_combined_v2/addAny_incorrect_pred.jsonl'
+ADD_COMMON_EVAL_OUTPUT_V2 = './new_eval_outputs/adv_combined_v2/addCommon_incorrect_pred.jsonl'
+
+
 
 # Load the eval_predictions.jsonl file
-dataset = load_dataset('json', data_files=ADD_COMMON_EVAL_INPUT, split='train')
+dataset = load_dataset('json', data_files=ADD_SENT_EVAL_INPUT_V2, split='train')
 
 # Function to remove trailing and leading punctuation from text
 def remove_punctuation(text):
@@ -30,7 +39,7 @@ def is_prediction_correct(example):
 incorrect_predictions = dataset.filter(lambda x: not is_prediction_correct(x))
 
 # Save incorrect predictions to a new file
-with open(ADD_COMMON_EVAL_OUTPUT, 'w') as f:
+with open(ADD_SENT_EVAL_OUTPUT_V2, 'w') as f:
     for example in incorrect_predictions:
         f.write(json.dumps(example) + '\n')
 
